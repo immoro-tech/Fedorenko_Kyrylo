63c846766c3b6acee2a9373533be50a9722161fc
lkkp.dll <73173101+immoro-tech@users.noreply.github.com>
2022-11-23 16:31:16 +0200

    Add files via upload

---
+++ Basic Knowledge/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Loop
+{
+    public static void getPairsCount(int[] arr, int sum)
+    {
+
+        int count = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+
+            for (int j = i + 1; j < arr.Length; j++)
+
+                if ((arr[i] + arr[j]) == sum)
+                    count++;
+
+        Console.WriteLine("Count of pairs is " + count);
+    }
+
+    static public void Main()
+    {
+
+        Console.Write("Entry number of array elements:\t");
+        
+        int elements = int.Parse(Console.ReadLine());
+        int[] arr = new int[elements];
+        
+        for (int i =0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"\nEntry array element at index {i}:\t");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        
+        Console.WriteLine("Entry sum; ");
+        int sum = Convert.ToInt32(Console.ReadLine());
+        
+        getPairsCount(arr, sum);
+    }
+}
