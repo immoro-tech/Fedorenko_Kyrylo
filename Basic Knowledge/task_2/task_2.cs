74d9bbaceaa698e4f0c3d98a251c2f2b01cda86d
lkkp.dll <73173101+immoro-tech@users.noreply.github.com>
2022-12-15 01:14:08 +0200

    Add files via upload

---
+++ Basic Knowledge/task_2/task_2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2;
+
+
+public class Loop
+{
+    public static char? FirstNonRepeat(string str)
+    {
+        Dictionary<char, int> countChars = new Dictionary<char, int>();
+        foreach (char symbol in str)
+        {
+            if (countChars.ContainsKey(char.ToLower(symbol)))
+            {
+                countChars[char.ToLower(symbol)]++;
+            }
+            else if (countChars.ContainsKey(char.ToUpper(symbol)))
+            {
+                countChars[char.ToUpper(symbol)]++;
+            }
+            else
+            {
+                countChars.Add(symbol, 1);
+            }
+        }
+        foreach (char symbol in str)
+        {
+            if (countChars[symbol] == 1)
+            {
+                return symbol;
+            }
+        }
+        return ' ';
+
+    }
+
+    static void Main()
+    {
+        Loop loop = new Loop();
+        string str = "sTreSS";
+        FirstNonRepeat(str);
+        Console.WriteLine(FirstNonRepeat(str));
+    }
+
+}
+
+       
